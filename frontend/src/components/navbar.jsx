feca24831ebc42b976c4578428c2420d24c0a574
kali <shubhh.ab@gmail.com>
2025-10-12 10:58:28 +0530

    heading

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -134,10 +134,10 @@ export default function Navbar() {
               className="flex items-center space-x-3 cursor-pointer group"
               onClick={() => navigate('/')}
             >
-              <OvercloakLogo className="w-8 h-8 drop-shadow-lg group-hover:scale-110 transition-transform duration-300" />
+          <img src="/vite.png" alt="PetConnect Logo" className='w-10 h-10 hover:scale-105' />
               {/* <span className="text-xl font-bold bg-gradient-to-r from-green-400 via-red-400 to-blue-400 bg-clip-text text-transparent"> */}
               <span className="text-xl font-bold bg-green-300 bg-clip-text text-transparent">
-                Code OF
+                Code OF Honour
               </span>
             </div>
 
